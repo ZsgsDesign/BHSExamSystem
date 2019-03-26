61b31b7478ecb2d5fe9b2358e10f026edaaad206
ZsgsDesign <zsgsdesign@gmail.com>
2019-03-27 07:35:49 +0800

    modify home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,6 +44,18 @@
         margin-bottom: 2rem;
     }
 
+    contest-card h5{
+        font-size: 1.2rem;
+        margin:0;
+    }
+
+    contest-card p{
+        font-size: 0.85rem;
+        margin:0;
+        color:rgba(0,0,0,0.54);
+        /* text-align: right; */
+    }
+
     contest-card score-section{
         display:flex;
         height:8rem;
@@ -86,23 +98,37 @@
     <div class="row">
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <contest-card>
-                <p><i class="MDI school"></i> 大学生诚信教育测试</p>
+                <h5><i class="MDI school"></i> 大学生诚信教育测试</h5>
                 <score-section>
                     <div>
-                        <current-score>98</current-score>
+                        <current-score class="wemd-green-text">98</current-score>
                         <tot-score>/ 100</tot-score>
                     </div>
                 </score-section>
+                <p><i class="MDI clock"></i> 2019年3月28日 截止</p>
             </contest-card>
         </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <contest-card>
-                <p><i class="MDI school"></i> 这是第二个测试</p>
+                <h5><i class="MDI school"></i> 这是第二个测试</h5>
                 <score-section>
                     <div>
                         <span>尚未作答</span>
                     </div>
                 </score-section>
+                <p><i class="MDI clock"></i> 2019年3月28日 截止</p>
+            </contest-card>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <contest-card>
+                <h5><i class="MDI school"></i> 大学生第三个测试</h5>
+                <score-section>
+                    <div>
+                        <current-score class="wemd-red-text">86</current-score>
+                        <tot-score>/ 100</tot-score>
+                    </div>
+                </score-section>
+                <p><i class="MDI clock"></i> 2019年3月28日 截止</p>
             </contest-card>
         </div>
     </div>
