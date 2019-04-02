193353909778b8a9e46ed30f489e7aae87ec6139
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-03 01:30:58 +0800

    update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\TextModel;
+use App\Models\TestModel;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -11,8 +11,8 @@ class TestController extends Controller
 {
     public function detail($tid)
     {
-        $textModel= new TextModel();
-        $testProb=$textModel->getProb($tid);
+        $testModel= new TestModel();
+        $testProb=$testModel->getProb($tid);
         return view('test.detail', [
             'page_title'=>"测试",
             'site_title'=>"贝尔英才学院诚信考试系统",
---
+++ app/Models/TestModel.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class TestModel extends Model
+{
+    protected $tableName='test';
+
+    public function getProb($tid)
+    {
+        $list=DB::table("test_problem")->where(["tid"=>$tid])->join("problem","problem.pid","=","test_problem.pid")->orderBy('pcode', 'asc')->select("test_problem.pid as pid","pcode","desc","choiceA","choiceB","choiceC","choiceD")->get()->all();
+        foreach($list as &$l){
+            $l["choices"]=[];
+            if(!is_null($l["choiceA"])){
+                $l["choices"][]=[
+                    "content"=>$l["choiceA"],
+                    "md5"=>md5($l["choiceA"])
+                ];
+            }
+            if(!is_null($l["choiceB"])){
+                $l["choices"][]=[
+                    "content"=>$l["choiceB"],
+                    "md5"=>md5($l["choiceB"])
+                ];
+            }
+            if(!is_null($l["choiceC"])){
+                $l["choices"][]=[
+                    "content"=>$l["choiceC"],
+                    "md5"=>md5($l["choiceC"])
+                ];
+            }
+            if(!is_null($l["choiceD"])){
+                $l["choices"][]=[
+                    "content"=>$l["choiceD"],
+                    "md5"=>md5($l["choiceD"])
+                ];
+            }
+            shuffle($l["choices"]);
+        }
+        return $list;
+    }
+}
