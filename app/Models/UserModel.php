4690d049b481a77f79cc77c64d653a8d1137e2ba
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-12 10:41:02 +0800

    add user manage

---
+++ app/Models/UserModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class UserModel extends Model
+{
+    public $table="users";
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable=[
+        'name', 'email', 'password', 'avatar', 'contest_account'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden=[
+        'password', 'remember_token',
+    ];
+
+    public static function findExam($uid,$eid){
+        $basic=DB::table("test")->where(["eid"=>$eid,"uid"=>$uid])->orderBy('score', 'desc')->first();
+        return empty($basic)?"/":$basic["score"];
+    }
+
+    public static function exams(){
+        return DB::table("exam")->where(["available"=>1])->get()->all();
+    }
+}
